1f6da0f167359f7332ef45f00129bada826b566a
EnglishCNTT <ngocanha3k52@gmail.com>
2023-03-17 18:45:39 +0700

    add file

---
+++ B5_DOM&JQuery/BT/BT1/script.js
@@ -0,0 +1,31 @@
+const btnBMI = $("#calculator")
+
+btnBMI.click(function () {
+    var height = $("#height").val();
+    var weight = $("#weight").val();
+    var status = "";
+    var result = (weight / (height * height)).toFixed(2);
+    if (getGender() == 'Nam') {
+        status += result < 20.7 ? "Gầy" :
+            result >= 20.7 && result < 26.4 ? "Bình thường" :
+                result >= 26.4 && result < 27.8 ? "Thừa cân nhẹ" :
+                    result >= 26.4 && result < 31.1 ? "Thừa cân nhẹ" : "Béo phì"
+    }
+    else {
+        status += result < 19.1 ? "Gầy" :
+            result >= 19.1 && result < 25.8 ? "Bình thường" :
+                result >= 25.8 && result < 27.3 ? "Thừa cân nhẹ" :
+                    result >= 27.3 && result < 32.3 ? "Thừa cân nhẹ" : "Béo phì"
+    }
+    $("#resultBmi").text(result + " | " + status)
+})
+
+function getGender() {
+    var gender = document.getElementsByName('gender');
+    for (var i = 0; i < gender.length; i++) {
+        if (gender[i].checked)
+            return gender[i].value
+    }
+}
+
+
---
+++ B5_DOM&JQuery/BT/BT2/script.js
@@ -0,0 +1,21 @@
+const btnCal = $(".btn");
+
+btnCal.click(function () {
+
+    const loan = $("#loan").val();
+    const interest = $("#interest").val() / 100 / 12;
+    const time = $("#time").val() * 12;
+
+    const x = Math.pow(1 + interest, time);
+    const monthlyPayment = ((loan * x * interest) / (x - 1));
+    const totalToPay = monthlyPayment * time;
+    const totalInterest = totalToPay - loan;
+
+    const output_MonthlyPayment = $("#output1");
+    const output_TotalToPay = $("#output2");
+    const output_TotalInterest = $("#output3");
+
+    output_MonthlyPayment.text(monthlyPayment.toFixed(2))
+    output_TotalToPay.text(totalToPay.toFixed(2))
+    output_TotalInterest.text(totalInterest.toFixed(2))
+})
