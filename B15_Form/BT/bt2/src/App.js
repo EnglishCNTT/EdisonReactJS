8227c8b2aae6b05e635f5233adf4292789106a7e
EnglishCNTT <ngocanha3k52@gmail.com>
2023-05-06 01:42:35 +0700

    doneb16

---
+++ B15_Form/BT/bt2/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import EditEmail from './editEmail';
+
+function App() {
+  return (
+    <div className="App">
+      <EditEmail />
+    </div>
+  );
+}
+
+export default App;
